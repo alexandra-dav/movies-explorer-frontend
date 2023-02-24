7a0bcc7fa56f41be54f1e007e9b4835ddada083a
Alexandra Davydova <davydovaalexandra225@gmail.com>
2023-02-24 18:24:03 +0300

    start

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,15 +4,15 @@
 
 import logo from '../../images/landing-logo.svg'
 export function Main() {
-    return (
-      <section className="landing">
-        <div className="landing__content" aria-label="Стартовая страницв">
-            <img src={logo} alt="Планета ВЕБ" className="landing__logo" />
-            <h1 className="landing__title">Учебный проект студента факультета Веб-разработки.</h1>
-            <p className="landing__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-            <button aria-label="navigation" className="landing__button">Узнать больше</button>
-        </div>
-      </section>
-    );
+  return (
+    <section className="landing">
+      <div className="landing__content" aria-label="Стартовая страницв">
+          <img src={logo} alt="Планета ВЕБ" className="landing__logo" />
+          <h1 className="landing__title">Учебный проект студента факультета Веб-разработки.</h1>
+          <p className="landing__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+          <button aria-label="navigation" className="landing__button">Узнать больше</button>
+      </div>
+    </section>
+  );
 }
   
