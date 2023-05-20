36da6e97cdd2f30c8a03aa8f5d4a72f8834532e8
Alexandra Davydova <davydovaalexandra225@gmail.com>
2023-05-20 18:08:44 +0300

    fixed: chenged protect rout

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, loggedIn }) => {
   if (!loggedIn) {
-    return <Navigate replace to="/signin" />;
+    return <Navigate replace to="/" />;
   }
   return children;
 };
